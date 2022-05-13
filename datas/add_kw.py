61a91bb9340f783cb0423b479a12c4dcf9afab9c
Dreamcats <maifeng868@gmail.com>
2022-05-13 11:23:53 +0800

    feat: update kw mj

--- datas/add_kw.py
+++ datas/add_kw.py
@@ -3,7 +3,7 @@ import os, pymysql, uuid, time
 db = pymysql.connect(
     host='113.31.114.17',
     user='root',
-    password='xxxx',
+    password='',
     db='wx_interview',
     port=3306,
     charset='utf8'
@@ -38,17 +38,20 @@ def transferContent(content):
     return stri
 
 m = {
-    '1':{'uuid':'6b4fd669bf2d4be59bcb74f6492e5515'},
-    '2':{'uuid':'e1468e42ce1a43d3aced7f8b3ae97e32'},
-    '3':{'uuid':'d2b77a758936416480004185bfe396d5'},
-    '4':{'uuid':'3caa232689284d05a99f1aeeff79858a'},
-    '5':{'uuid':'43371b5675a74549833ade74d76c0a24'},
-    '6':{'uuid':'bd6b64f0490a4c3db78054257c4daf7a'},
-    '7':{'uuid':'7464184961714b6893e7151216d1691a'},
-    '8':{'uuid':'85d217125843478b8f21537e5377157f'},
-    '9':{'uuid':'36f25136333a43ab88b496d24107d155'},
-    '10':{'uuid':'b8e313c1b97e48adbdbd15d682f65b71'},
-    '11':{'uuid':'d8573717569347758a4197a44ea3e24d'},
+    '1':{'uuid':'6b4fd669bf2d4be59bcb74f6492e5515', 'tag_type': 1},
+    '2':{'uuid':'e1468e42ce1a43d3aced7f8b3ae97e32', 'tag_type': 1},
+    '3':{'uuid':'d2b77a758936416480004185bfe396d5', 'tag_type': 1},
+    '4':{'uuid':'3caa232689284d05a99f1aeeff79858a', 'tag_type': 3},
+    '5':{'uuid':'43371b5675a74549833ade74d76c0a24', 'tag_type': 1},
+    '6':{'uuid':'bd6b64f0490a4c3db78054257c4daf7a', 'tag_type': 1},
+    '7':{'uuid':'7464184961714b6893e7151216d1691a', 'tag_type': 2},
+    '8':{'uuid':'85d217125843478b8f21537e5377157f', 'tag_type': 2},
+    '9':{'uuid':'36f25136333a43ab88b496d24107d155', 'tag_type': 2},
+    '10':{'uuid':'b8e313c1b97e48adbdbd15d682f65b71', 'tag_type': 2},
+    '11':{'uuid':'d8573717569347758a4197a44ea3e24d', 'tag_type': 2},
+    '12':{'uuid':'fdc2a939a72846458cf592f8fcbc291c', 'tag_type': 4},
+    '13':{'uuid':'0199aac3351a442a94efa37d26c11efd', 'tag_type': 4},
+    '14':{'uuid':'2bc32089dabd4cf2a4e7c3698c81b7c5', 'tag_type': 4}
 }
 
 def getUUID(name):
@@ -61,15 +64,28 @@ files_list = load_files()
 for f in files_list:
     print(f)
     content = transferContent(get_text(f))
-    tag_id = int(f.split('/')[1]) + 1
+    try:
+        tag_id = int(f.split('/')[1]) + 1
+    except Exception as e:
+        # print(e)
+        continue
     title = f.split('/')[-1].split('.md')[0]
     s_id = title.split('.')[0]
+    tag_type = m[str(tag_id)]['tag_type']
     tc_uuid = m[str(tag_id)]['uuid']
-    uuid_bak = getUUID(title)
+    uuid_bak = getUUID(f'{tag_id}-{s_id}-{tag_type}')
     publish_time = getTime()
     print(uuid_bak)
     url = f
     with db.cursor() as conn:
-        sql = f'''INSERT INTO wx_items(uuid, tc_uuid, s_id, title, publish_time, url, content) VALUES("{uuid_bak}", "{tc_uuid}", {int(s_id)}, "{title}", "{publish_time}", "{url}", "{content}")'''
-        conn.execute(sql)
+        sql_search = f'''select id from wx_items where uuid = "{uuid_bak}"'''
+        res_search = conn.execute(sql_search)
+        if res_search == 0:
+            # 不存在
+            sql_insert = f'''INSERT INTO wx_items(uuid, tc_uuid, s_id, tag_type, title, publish_time, url, content) VALUES("{uuid_bak}", "{tc_uuid}", {int(s_id)}, {tag_type}, "{title}", "{publish_time}", "{url}", "{content}")'''
+            res_insert = conn.execute(sql_insert)
+        else:
+            # 存在, update
+            sql_update = f'''update wx_items set title="{title}", publish_time="{publish_time}", content="{content}" where uuid="{uuid_bak}"'''
+            res_update = conn.execute(sql_update)
         db.commit()
--- datas/spider_mj.py
+++ datas/spider_mj.py
@@ -6,31 +6,31 @@ s = requests.session()
 db = pymysql.connect(
     host='113.31.114.17',
     user='root',
-    password='xxx',
+    password='',
     db='wx_interview',
     port=3306,
     charset='utf8'
 )
 # 修改c_id参数
 cs = {
-    '1': {'tagId':665, 'expTag':644, 'uuid': '3dd371f979db41c786606eeda47580c4'}, # 字节前端 13
-    '2': {'tagId':665, 'expTag':639, 'uuid': 'a0adad4c677f43e79d200ae4b864a37c'}, # 字节后端 26
-    '3': {'tagId':134, 'expTag':644, 'uuid': '2f0440dbe491437487a5e096edf2a355'}, # 阿里前端 7
-    '4': {'tagId':134, 'expTag':639, 'uuid': 'f8ca2500d425483f8891bbe590248616'}, # 阿里后端 26
-    '5': {'tagId':138, 'expTag':644, 'uuid': '7a89cff5df2d4ebb9993d0ba986c6d98'}, # 腾讯前端 12
-    '6': {'tagId':138, 'expTag':639, 'uuid': '8cb8d566f9e24198935c382b2d1d56cd'}, # 腾讯后端 19
-    '7': {'tagId':898, 'expTag':644, 'uuid': '933e5d7e8c224ab1ad663f5dba6f3b64'}, # 快手前端 3
-    '8': {'tagId':898, 'expTag':639, 'uuid': '3695a49d5534464d85131e3f22318f7d'}, # 快手后端 9
-    '9': {'tagId':151, 'expTag':644, 'uuid': '626bd457b5ee4db1914f8a87e9c9944f'}, # 京东前端 5
-    '10': {'tagId':151, 'expTag':639, 'uuid': '4932c5cd44ff405d82b60b4021b1f76c'}, # 京东后端 13
-    '11': {'tagId':732, 'expTag':644,'uuid': 'b2079ebe32a94537ba0e6669b42c5496'}, # 拼多多前端 2
-    '12': {'tagId':732, 'expTag':639,'uuid': '9e3c9366a4114d41a432f2c4fba1f801'}, # 拼多多后端 5
-    '13': {'tagId':179, 'expTag':644,'uuid': 'b7a0b038c5ba44c4b39576e9a89e4a3f'}, # 美团前端 9
-    '14': {'tagId':179, 'expTag':639,'uuid': '3891f18847a940a299851a471848754f'}, # 美团后端 22
-    '15': {'tagId':139, 'expTag':644,'uuid': 'a6543a816024486cb863c7e1695abb66'}, # 百度前端 5
-    '16': {'tagId':139, 'expTag':639,'uuid': 'a6543a816024486cb863c7e1695abb65'}, # 百度后端 10
-    '17': {'tagId':149, 'expTag':644,'uuid': 'b3fafc7cbaaa44aabc807cb95e7f325c'}, # 网易前端 5
-    '18': {'tagId':149, 'expTag':639,'uuid': 'b3fafc7cbaaa44aabc807cb95e7f3251'}, # 网易后端 11
+    '1': {'tagId':665, 'expTag':644, 'uuid': '3dd371f979db41c786606eeda47580c4', 'tag_type': 0}, # 字节前端 13
+    '2': {'tagId':665, 'expTag':639, 'uuid': 'a0adad4c677f43e79d200ae4b864a37c', 'tag_type': 0}, # 字节后端 26
+    '3': {'tagId':134, 'expTag':644, 'uuid': '2f0440dbe491437487a5e096edf2a355', 'tag_type': 0}, # 阿里前端 7
+    '4': {'tagId':134, 'expTag':639, 'uuid': 'f8ca2500d425483f8891bbe590248616', 'tag_type': 0}, # 阿里后端 26
+    '5': {'tagId':138, 'expTag':644, 'uuid': '7a89cff5df2d4ebb9993d0ba986c6d98', 'tag_type': 0}, # 腾讯前端 12
+    '6': {'tagId':138, 'expTag':639, 'uuid': '8cb8d566f9e24198935c382b2d1d56cd', 'tag_type': 0}, # 腾讯后端 19
+    '7': {'tagId':898, 'expTag':644, 'uuid': '933e5d7e8c224ab1ad663f5dba6f3b64', 'tag_type': 0}, # 快手前端 3
+    '8': {'tagId':898, 'expTag':639, 'uuid': '3695a49d5534464d85131e3f22318f7d', 'tag_type': 0}, # 快手后端 9
+    '9': {'tagId':151, 'expTag':644, 'uuid': '626bd457b5ee4db1914f8a87e9c9944f', 'tag_type': 0}, # 京东前端 5
+    '10': {'tagId':151, 'expTag':639, 'uuid': '4932c5cd44ff405d82b60b4021b1f76c', 'tag_type': 0}, # 京东后端 13
+    '11': {'tagId':732, 'expTag':644,'uuid': 'b2079ebe32a94537ba0e6669b42c5496', 'tag_type': 0}, # 拼多多前端 2
+    '12': {'tagId':732, 'expTag':639,'uuid': '9e3c9366a4114d41a432f2c4fba1f801', 'tag_type': 0}, # 拼多多后端 5
+    '13': {'tagId':179, 'expTag':644,'uuid': 'b7a0b038c5ba44c4b39576e9a89e4a3f', 'tag_type': 0}, # 美团前端 9
+    '14': {'tagId':179, 'expTag':639,'uuid': '3891f18847a940a299851a471848754f', 'tag_type': 0}, # 美团后端 22
+    '15': {'tagId':139, 'expTag':644,'uuid': 'a6543a816024486cb863c7e1695abb66', 'tag_type': 0}, # 百度前端 5
+    '16': {'tagId':139, 'expTag':639,'uuid': 'a6543a816024486cb863c7e1695abb65', 'tag_type': 0}, # 百度后端 10
+    '17': {'tagId':149, 'expTag':644,'uuid': 'b3fafc7cbaaa44aabc807cb95e7f325c', 'tag_type': 0}, # 网易前端 5
+    '18': {'tagId':149, 'expTag':639,'uuid': 'b3fafc7cbaaa44aabc807cb95e7f3251', 'tag_type': 0}, # 网易后端 11
     # '20': {'tagId':147, 'expTag':644,'tag_id': 45}, # 小米前端 4
     # '21': {'tagId':147, 'expTag':639,'tag_id': 38}, # 小米后端 8
     # '22': {'tagId':239, 'expTag':644,'tag_id': 41}, # 华为前端 1
@@ -101,6 +101,7 @@ def spider(url, c_id):
             'publish_time': t,
             'url': f'https://www.nowcoder.com/discuss/{value["postId"]}',
             'uuid': getUUID(url),
+            'tag_type': cs[c_id]['tag_type']
         }
         res = s.get(url=url)
         doc = pq(res.text)
@@ -127,10 +128,10 @@ def run():
     print('今天抓取结束...')
 
 if __name__ == "__main__":
-    # run()
-    print("每日定时抓取面经存到数据库中...")
-    schedule.every().day.at("01:00").do(run)
-    while True:
-        schedule.run_pending()   # 运行所有可以运行的任务
-        time.sleep(2)
+    run()
+    # print("每日定时抓取面经存到数据库中...")
+    # schedule.every().day.at("01:00").do(run)
+    # while True:
+    #     schedule.run_pending()   # 运行所有可以运行的任务
+    #     time.sleep(2)
 
